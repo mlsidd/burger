f387a0b8aa222827bbc8106885ae1aa4b550bc69
Mel Sidd <mlsiddoway@gmail.com>
2018-08-10 18:13:08 -0500

    added jawsdb environmental variable

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,8 @@ var mysql = require("mysql");
 var connection;
 
 if(process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
-
-    
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {    
     connection = mysql.createConnection({
     host: "localhost",
     port: 8080,
